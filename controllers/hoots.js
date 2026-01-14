f5d1f8d8ff73ae594bbe5152c308356d1632752a
Sarah Smith <sarahlib90@gmail.com>
2026-01-14 15:49:25 -0500

    Finished full CRUD routes

--- controllers/hoots.js
+++ controllers/hoots.js
@@ -18,4 +18,101 @@ router.post('/', verifyToken, async (req, res) => {
     }
 });
 
+// GET /hoots (A user needs to be logged in to view a list of hoots, so be sure to include the verifyToken middleware)
+router.get('/', verifyToken, async(req, res) => {
+    try {
+        const hoots = await Hoot.find({})
+        .populate('author')
+        .sort({ createdAt: 'desc' });
+    res.status(200).json(hoots);
+
+    } catch (err) {
+        res.status(500).json({ err: err.message });
+    }
+});
+
+// GET /:hootId
+router.get("/:hootId", verifyToken, async (req, res) => {
+  try {
+    const hoot = await Hoot.findById(req.params.hootId).populate(['author', 'comments.author']);
+    res.status(200).json(hoot);
+  } catch (err) {
+    res.status(500).json({ err: err.message });
+  }
+});
+
+// PUT /hoots/:hootId
+router.put('/:hootId', verifyToken, async(req, res) => {
+    try {
+        // find the hoot
+        const hoot = await Hoot.findById(req.params.hootId);
+        // check if hoot exists
+        if (!hoot) {
+            return res.status(404).json({ err: 'Hoot not found.' });
+        }
+        // check permissions
+        if (!hoot.author.equals(req.user._id)) {
+            return res.status(403).send('Access denied!');
+        }
+        // update hoot
+        const updatedHoot = await Hoot.findByIdAndUpdate(
+            req.params.hootId,
+            req.body, 
+            { new: true }
+        );
+        // append user to the author property
+        updatedHoot._doc.author = req.user;
+        // issue json response
+        res.status(200).json(updatedHoot);
+    } catch (err) {
+        res.status(500).json({ err: err.message });
+    }
+});
+
+// DELETE /hoots/:hootId
+router.delete("/:hootId", verifyToken, async (req, res) => {
+  try {
+    const hoot = await Hoot.findById(req.params.hootId);
+
+    if (!hoot) {
+        return res.status(404).json({ err: 'Hoot not found.' });
+    }
+
+    if (!hoot.author.equals(req.user._id)) {
+      return res.status(403).send("You're not allowed to do that!");
+    }
+
+    const deletedHoot = await Hoot.findByIdAndDelete(req.params.hootId);
+    res.status(200).json(deletedHoot);
+  } catch (err) {
+    res.status(500).json({ err: err.message });
+  }
+});
+
+// POST /hoots/:hootId/comments
+router.post('/:hootId/comments', verifyToken, async(req, res) => {
+    try {
+        // author of comment
+        req.body.author = req.user._id;
+        // find hoot to add comment to
+        const hoot = await Hoot.findById(req.params.hootId);
+
+        if (!hoot) {
+            return res.status(404).json({ err: 'Hoot not found.' });
+        }
+        // push comments into hoot via the form
+        hoot.comments.push(req.body);
+        await hoot.save();
+
+        // find newly created comment from array of comments
+        const newComment = hoot.comments[hoot.comments.length - 1];
+        newComment._doc.author = req.user;
+
+        // respond with the newComment
+        res.status(201).json(newComment);
+    } catch (err) {
+        res.status(500).json({ err: err.message });
+    }
+});
+
 module.exports = router;
